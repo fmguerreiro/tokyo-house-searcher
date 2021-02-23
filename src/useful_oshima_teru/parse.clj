e93e6e46be8be69b4a9103068089bd9605a8c11c
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2021-02-23 22:21:25 +0900

    Scrape each listing for each page

--- src/useful_oshima_teru/parse.clj
+++ src/useful_oshima_teru/parse.clj
@@ -6,12 +6,27 @@
 (def keyword (codec/percent-encode "告知事項"))
 (def url (str "https://suumo.jp/jj/chintai/ichiran/FR301FC011/?ar=030&bs=040&kskbn=01&fw=" keyword))
 
-(let [data (enlive/html-resource (java.net.URL. url))
-      pages (enlive/select data [:div #{:.pagination :.pagination_set-nav}])
-      n-pages (->> pages
-                   (enlive/select pages [:a :> enlive/text-node])
+#_(
+   (def data (enlive/html-resource (java.net.URL. url)))
+   (def pages (enlive/select data [:div #{:.pagination :.pagination_set-nav}]))
+   (def n-pages (->> (enlive/select pages [:a :> enlive/text-node])
+                     (filter #(not= "次へ" %))
+                     (map #(Integer/parseInt %))
+                     (apply max)))
+   (def urls (map #(str url "&pn=" (+ % 2)) (range (- n-pages 1))))
+
+   )
+
+(defn- scrape [url enlive-exp]
+  (let [data (enlive/html-resource (java.net.URL. url))]
+    (enlive/select data enlive-exp)))
+
+(let [pages (scrape url [:div #{:.pagination :.pagination_set-nav}])
+      n-pages (->> (enlive/select pages [:a :> enlive/text-node])
                    (filter #(not= "次へ" %))
                    (map #(Integer/parseInt %))
                    (apply max))
-      urls (map #(str url "&pn=" (+ % 2) (range (- n-pages 1)))]
+      urls (map #(str url "&pn=" (+ % 2)) (range (- n-pages 1)))
+      listings (->> urls
+                    (pmap #(scrape % [:div#js-bukkenList])))]
   (map #(-> %) pages))
