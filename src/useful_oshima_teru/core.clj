0324556aceb8454575ed7442dabf2a54e386ce0f
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2021-03-18 23:10:10 +0900

    Add user input parsing

--- src/useful_oshima_teru/core.clj
+++ src/useful_oshima_teru/core.clj
@@ -1,5 +1,9 @@
 (ns useful-oshima-teru.core
-    (:require [useful-oshima-teru.stream :as stream])
+  (:require [useful-oshima-teru.stream :as stream]
+            [useful-oshima-teru.parse :as parse]
+            [clojure.tools.cli :refer [cli]]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn])
     (:gen-class))
 
 (defn parse-args [args]
@@ -13,6 +17,46 @@
        parse-line
        println))
 
+(defn- load-edn [source]
+  (try
+    (with-open [r (io/reader source)]
+      (edn/read (java.io.PushbackReader. r)))
+    (catch java.io.IOException e
+      (printf "Couldn't open '%s': %s\n" source (.getMessage e)))
+    (catch RuntimeException e
+      (printf "Error parsing edn file '%s': %s\n" source (.getMessage e)))))
+
+(defn- write-file [data file]
+  (let [f (io/file file)]
+    (-> f
+        (.getAbsoluteFile)
+        (.getParentFile)
+        (.mkdirs))
+    (with-open [wrtr (io/writer f)]
+      (dorun
+       (map #(do (.write wrtr (str %)))
+            data)))))
+
+(defn- write-stdout [data]
+  (dorun (map #(println %) data)))
+
+(defn- do-run
+  [in out]
+  (let [conf (load-edn in)
+        res ()] ; todo parse suumo and filter based on conf
+    (if out
+      (write-file res out)
+      (write-stdout res))))
+
 (defn -main [& args]
-  (let [index (parse-args args)]
-    (stream/mapper *in* print-line)))
+  (let [[options args banner]
+        (cli args
+             "Useful house search alert."
+             ["-i" "File where to read configuration from."]
+             ["-o" "File where to store the results. Defaults to stdout."]
+             ["-h" "--help" "Shows this help" :flag true])]
+    (cond
+      (:help options) (println banner)
+      :else (do-run
+             (:i options)
+             (:o options)))))
