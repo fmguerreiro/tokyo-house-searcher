4508c16f0ea8e894526e13ffcea51d026e98d958
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2021-08-13 13:00:49 +0900

    Fix listing results (interleaving was incorrect)

--- src/useful_oshima_teru/parse.clj
+++ src/useful_oshima_teru/parse.clj
@@ -8,86 +8,62 @@
 (def ^:dynamic *filter* "告知事項")
 (def ^:dynamic *base-url* (str "https://suumo.jp/jj/chintai/ichiran/FR301FC011/?ar=030&bs=040&kskbn=01&fw=" (codec/percent-encode *filter*)))
 (def ^:dynamic *host* (let [split (str/split *base-url* #"/")]
-            (str (split 0) "//" (split 2))))
+                        (str (split 0) "//" (split 2))))
 
 (def ^:dynamic *pagination-selector* [:div #{:.pagination :.pagination_set-nav} :a :> html/text-node])
+(def ^:dynamic *listing-selector* [:div [:.property :.property--highlight :.js-property :.js-cassetLink]])
 (def ^:dynamic *name-selector* [:h2.property_inner-title :.js-cassetLinkHref :> html/text-node])
-(def ^:dynamic *link-selector* [:h2.property_inner-title :.js-cassetLinkHref])
-(def ^:dynamic *price-selector* [#{:div.detailbox-property-point (html/right :div.detailbox-property-point) } :> html/text-node]) ; TODO: make it grep 管理費 as well
+(def ^:dynamic *link-selector* [:h2.property_inner-title :> :a])
+(def ^:dynamic *price-selector* [#{:div.detailbox-property-point (html/right :div.detailbox-property-point)} :> html/text-node]) ; TODO: make it grep 管理費 as well
 (def ^:dynamic *size-selector* [:td.detailbox-property--col3 [:div (html/nth-of-type 2)] :> [html/text-node (html/text-pred #(re-matches #".*m$" %))]])
 (def ^:dynamic *distance-selector* [[:div.detailnote-box (html/nth-of-type 1)] :div :> html/text-node])
 
 (util/defn-memo fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn- scrape [url enlive-exp]
+(defn- select [url enlive-exp]
   (let [data (fetch-url url)]
     (html/select data enlive-exp)))
 
 (defn- get-page-count [url]
-  (let [pages (scrape *base-url* *pagination-selector*)]
+  (let [pages (select url *pagination-selector*)]
     (->> pages
          (filter #(not= "次へ" %))
          (map #(Integer/parseInt %))
          (apply max))))
 
-(defn- extract-numbers [s]
-  (edn/read-string ((re-find #"[+-]?([0-9]*[.])?[0-9]+" s) 0)))
+(defn- get-listings [url]
+  (select url *listing-selector*))
 
-(defn- parse-distance [s] ;;=> "ＪＲ東海道本線/国府津駅 歩15分"
-  (let [split (str/split s #" ")
-        walking (extract-numbers (split 1))
-        from (split 0)]
-    {:walking (if (str/includes? (split 1) "バス") (* 2 walking) walking)
-     :from from})
-  ) ;; => {:walking "15" :from "国府津駅"}
+(defn- listing->map [l]
+  (let [name (first (html/select l *name-selector*))
+        price (first (html/select l *price-selector*))
+        distance (first (html/select l *distance-selector*))
+        size (first (html/select l *size-selector*))
+        link (str *host* (-> (html/select l *link-selector*) first :attrs :href))]
+    {:name name, :price price, :distance distance, :size size, :link link}))
 
-(defn- parse-amount [s]
-  (* 10000 (extract-numbers s)))
+;; TODO finish and test
+;; (defn- extract-numbers [s]
+;;   (edn/read-string ((re-find #"[+-]?([0-9]*[.])?[0-9]+" s) 0)))
 
-(defn scrape-listings [url]
-  (let [names  (scrape url *name-selector*)
-        prices (map #(parse-amount %) (scrape url *price-selector*))
-        sizes  (map #(extract-numbers %) (scrape url *size-selector*))
-        dists  (map #(parse-distance %) (scrape url *distance-selector*))
-        links  (map #(str *host* (:href (:attrs %))) (scrape url *link-selector*))]
-    (->> (interleave names prices sizes dists links)
-         (partition 5)
-         (map #(zipmap [:name :price :size :distance :link] %)))))
+;; (defn- parse-distance [s] ;;=> "ＪＲ東海道本線/国府津駅 歩15分"
+;;   (let [split (str/split s #" ")
+;;         walking (extract-numbers (split 1))
+;;         from (split 0)]
+;;     {:walking (if (str/includes? (split 1) "バス") (* 2 walking) walking)
+;;      :from from})) ;; => {:walking "15" :from "国府津駅"}
 
-(defn get-listings [url]
-  (->> url
-       (pmap scrape-listings)))
+;; (defn- parse-amount [s]
+;;   (* 10000 (extract-numbers s)))
 
 (defn parse
   ([]    (parse *base-url*))
   ([url] (let [n-pages (get-page-count url)
-               urls (map #(str url "&pn=" (+ % 2)) (range (- n-pages 1)))]
-           (flatten (map #(scrape-listings %) urls)))))
+               urls (map #(str url "&pn=" (+ % 2)) (range (- n-pages 1)))
+               listings-per-url (pmap #(get-listings %) urls)
+               listings (flatten listings-per-url)
+               res (map #(listing->map %) listings)]
+           res)))
 
-#_(
-   (def data (html/html-resource (java.net.URL. *base-url*)))
-   (def pages (html/select data *pagination-selector*))
-   (def n-pages (->> pages
-                     (filter #(not= "次へ" %))
-                     (map #(Integer/parseInt %))
-                     (apply max)))
-   (def urls (map #(str *base-url* "&pn=" (+ % 2)) (range (- n-pages 1))))
-   (def urls [(first urls)])
-   (def url "https://suumo.jp/jj/chintai/ichiran/FR301FC011/?ar=030&bs=040&kskbn=01&fw=%E5%91%8A%E7%9F%A5%E4%BA%8B%E9%A0%85&pn=2")
-   (def ^:dynamic *link-selector* [:h2.property_inner-title :.js-cassetLinkHref])
-   (map #(:href (:attrs %)) (scrape url *link-selector*))
-   (def listings (->> urls   ;; => ["https://suumo.jp/jj/chintai/ichiran/FR301FC011/?ar=030&bs=040&kskbn=01&fw=%E5%91%8A%E7%9F%A5%E4%BA%8B%E9%A0%85&pn=2"]
-                      (pmap (fn [url] (scrape url *station-distance-selector*)))))
-   (scrape-listings url)
-   (zipmap '(:name :price :size :distance) '("a" "b" "c" "d"))
-   (->> urls (pmap scrape-listings))
-   (doall listings)
-   (def a (parse *base-url*))
-   (count (flatten a))
-   (first a);; => {:name "国府津 1K 1階",
-            ;;     :price "2.4万円",
-            ;;     :size "16.66m",
-            ;;     :distance "ＪＲ東海道本線/国府津駅 歩15分",
-            ;;     :link "https://suumo.jp/chintai/bc_100215472530/"}
-   )
+#_()
