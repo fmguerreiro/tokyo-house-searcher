8937f56442c208d5ace337657c106128c2a5afae
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2023-11-04 20:01:23 +0900

    feat: add email body creation

--- src/app/email.clj
+++ src/app/email.clj
@@ -1,5 +1,6 @@
 (ns app.email
   (:require [postal.core :as postal]
+            [clojure.string :as str]
             [dotenv :refer [env]]))
 
 (defn send-email
@@ -14,3 +15,19 @@
                         :to (env :EMAIL_USER)
                         :subject subject
                         :body body}))
+
+(defn create-msg-body
+  [results]
+  (let [n     (count results)
+        data  (map #(let [name  (str (get-in % [:data :houses/id]))
+                          price (str (get-in % [:data :houses/price]))
+                          size  (str (get-in % [:data :houses/size]))
+                          link  (str (get-in % [:data :houses/link]))]
+                      (str "House " name "\n" "Price: " price " Size: " size " Link: " link "\n"))
+                   results)]
+    (str "Found " n " outliers:\n" (str/join "\n" data))))
+
+#_(
+   (def res '({:diff 47.986851375274355, :i 8324, :data #:houses{:id "広尾ガーデンヒルズ　Ｍ棟", :price 70.0, :size 86.0, :location "渋谷", :transportation "東京メトロ日比谷線/広尾駅 歩5分", :building_age 40, :building_floor "地下1地上7階建", :link "/chintai/jnc_000084485069/?bc=100344577164"}}))
+   (create-msg-body res)
+   )
--- src/app/scraper.clj
+++ src/app/scraper.clj
@@ -1,12 +1,21 @@
 (ns app.scraper
   (:require [app.scraper.parse :as parse]
+            [app.predictor.main :as predictor]
+            [app.email :as email]
             [app.db :as db]))
 
 (defn -main
   [& args]
   (println "Scraping suumo!")
   (map #(db/insert %) (parse/scrape-suumo))
-  (println "Done!"))
+  (println "Scraping done!")
+  (let [results (predictor/find-outliers)]
+    (println "Predicting done!")
+    (println "Outliers:")
+    (println results)
+    (println "Sending email...")
+    (email/send-email "Recommended rentals" (email/create-msg-body results))
+    (println "Email sent!")))
 
 #_(
    (def res (parse/scrape-suumo))
